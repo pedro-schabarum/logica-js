cb09f35df5d9e5fae6491d7704d0ccfc4ebabd5b
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2023-10-26 10:40:22 -0300

    logica_js

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 14;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log(`Isso aí, você descobriu o ${númeroSecreto}!`);
+}
