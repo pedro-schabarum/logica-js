1b12da0041ce6e55f9ecb854a69216b2d8b86660
Pedro Guilherme Merisio Schabarum <pedro.guilherme0803@gmail.com>
2023-11-11 16:39:50 -0300

    nada alterado :)

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 14;
-let chute = prompt('Escolha um número entre 1 e 30');
+let NumeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random()*NumeroMaximo+1);
+let chute;
+let tentativas=1;
 
-if (numeroSecreto == chute) {
-    console.log(`Isso aí, você descobriu o ${númeroSecreto}!`);
+while (numeroSecreto!=chute){
+    chute = prompt(`Escolha um número entre 1 e ${NumeroMaximo}`);
+    if (numeroSecreto == chute) {
+        break;
+    } else { 
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
+    tentativas++;
 }
+
+let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+
+    alert(`Isso aí, você descobriu o ${numeroSecreto} em ${tentativas} ${palavraTentativa}.`);
